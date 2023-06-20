491804123dad67411e27d6d05b4d0f692fd39969
Adan Kvitschal <adan@moduhub.com>
2023-06-20 10:19:49 -0300

    New IndexedDB manager allows compatible stores also allow upgrading from previous versions

--- src/IndexedDBBase.js
+++ src/IndexedDBBase.js
@@ -8,22 +8,19 @@
 import {logger} from '@fieldfare/core';
 
 const dbname = 'fieldfare';
-const dbversion = 1;
+const dbversion = 3;
 
 export class IndexedDBBase {
 
-    constructor(storeName) {
-
-        if(IndexedDBBase.stores === null
-        || IndexedDBBase.stores === undefined) {
-            logger.log('info', "IndexedDBBase.stores = new Set();");
+    constructor(storeName, compatibleName) {
+        if(IndexedDBBase.stores === undefined
+        || IndexedDBBase.stores === null) {
+            logger.debug("IndexedDBBase.stores = new Set();");
             IndexedDBBase.stores = new Set();
         }
-
         this.storeName = storeName;
-
-        IndexedDBBase.stores.add(storeName);
-
+        this.compatibleName = compatibleName;
+        IndexedDBBase.stores.add({storeName, compatibleName, instance:this});
     }
 
     connect() {
@@ -37,59 +34,79 @@ export class IndexedDBBase {
     }
 
     static upgrade(event) {
-
         //indexedDB.deleteDatabase(this.dbname);
-
-        logger.log('info', "Upgrading DB: " + dbname + ' Version: ' + dbversion);
-
+        logger.debug("Upgrading DB: " + dbname + ' Version: ' + dbversion);
         var db = event.target.result;
-
-        // Stops Store Init
-        for(const storeName of IndexedDBBase.stores) {
-
-            logger.log('info', "Creating store: " + storeName);
-            db.createObjectStore(storeName);
-
+        for(const {storeName, compatibleName, instance} of IndexedDBBase.stores) {
+            if(!db.objectStoreNames.contains(storeName)) {
+                if(db.objectStoreNames.contains(compatibleName)) {
+                    instance.storeName = compatibleName;
+                    logger.debug("Using compatible store: \"" + compatibleName + "\" for store \"" + storeName + "\"");
+                } else {
+                    logger.debug("Creating store: " + storeName);
+                    db.createObjectStore(storeName);
+                }
+            } else {
+                logger.debug("Store already exists, keeping current: " + storeName);
+            }
         }
+    }
 
+    getStore(options) {
+        return this.connect()
+        .then((db) => {
+            try{
+    		    const tx = db.transaction(this.storeName, options);
+                const store = tx.objectStore(this.storeName);
+                return {db, store};
+            } catch(error) {
+                db.close();
+                if(error.name === 'NotFoundError'
+                && this.compatibleName
+                && this.compatibleName !== this.storeName) {
+                    logger.debug("Store " + this.storeName + " not found, retrying with compatible store: " + this.compatibleName);
+                    this.storeName = this.compatibleName;
+                    return this.getStore(options);
+                }
+                throw error;
+            }
+        });
     }
 
     put(key, entry) {
-
-        return this.connect()
-        .then((db) => {
+        return this.getStore('readwrite').then(({db, store}) => {
             return new Promise((resolve, reject) => {
-
-    			const tx = db.transaction(this.storeName, 'readwrite');
-    			const store = tx.objectStore(this.storeName);
     			const request = store.put(entry, key);
-
     			request.onsuccess = () => {
                     db.close();
                     resolve(request.result);
                 }
-
     			request.onerror = () => {
+                    logger.debug("IndexedDB error storing key: " + key);
                     db.close();
                     reject(request.error);
                 }
             });
 		});
-
 	}
 
     get(key) {
-        return this.connect()
-        .then((db) => {
+        return this.getStore('readonly').then(({db, store}) => {
             return new Promise((resolve, reject) => {
-                const tx = db.transaction(this.storeName, 'readonly');
-                const store = tx.objectStore(this.storeName);
                 const request = store.get(key);
                 request.onsuccess = () => {
                     db.close();
-                    resolve(request.result)
+                    if(request.result) {
+                        resolve(request.result);
+                    } else {
+                        const notFoundError = new Error("Key not found: " + key);
+                        notFoundError.name = 'NOT_FOUND';
+                        notFoundError.notFound = true;  //this is done to look like node level
+                        reject(notFoundError);
+                    }
                 };
                 request.onerror = () => {
+                    logger.debug("IndexedDB error getting key: " + key, request.error);
                     db.close();
                     reject(request.error);
                 };
